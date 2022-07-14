f43ee3352f64f2b725a816e9c17bf46048e44fd6
zestydevy <zestydevy@gmail.com>
2022-07-14 10:38:27 -0500

    decode functions. not matching with .rodata??

--- include/decode.h
+++ include/decode.h
@@ -6,13 +6,29 @@
 typedef enum
 {
     DECODE_NONE = 0,
-    DECODE_LZ,
-    DECODE_YAZ0,
-    DECODE_TYPE3,
-    DECODE_TYPE4,
-    DECODE_RLE
+    DECODE_LZ,              // lzss
+    DECODE_SLIDE,           // yaz0/szs
+    DECODE_FSLIDE,          // yaz0/szs, no loopback
+    DECODE_FSLIDE_2,        // yaz0/szs, no loopback
+    DECODE_RLE              // custom rle variant
 } EDecodeType;
 
+struct decode_struct {
+    u16 chunkLen;
+    s16 pad;
+    u8 * src;
+    u8 * dest;
+    u32 len;
+};
+
+extern u8 D_800ABFF0[1024]; // src copy
+extern u8 D_800AC3F0[1024]; // window
+
+void HuDecodeNone(struct decode_struct * decode);
+void HuDecodeLZ(struct decode_struct * decode);
+void HuDecodeSlide(struct decode_struct * decode);
+void HuDecodeFslide(struct decode_struct * decode);
+void HuDecodeRLE(struct decode_struct * decode);
 void HuDecode(void * src, void * dest, s32 len, EDecodeType decodeType);
 
 #endif
